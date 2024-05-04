0b84ea48159faa987f5381623ed12ce160633ff8
Gabriel Gaia <gflausinogaia@gmail.com>
2024-05-04 14:06:46 -0300

    Introdução do metodo area na classe triangulo

--- Triangulo/src/classes/Triangle.java
+++ Triangulo/src/classes/Triangle.java
@@ -4,8 +4,6 @@ public class Triangle {
 
     public double l1,l2,l3;
 
-    public double area;
-
     public double calculateArea () {
         double p = (l1 + l2 + l3)/2;
         return Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
--- Triangulo/src/programa/Teste.java
+++ Triangulo/src/programa/Teste.java
@@ -1,17 +1,52 @@
 package programa;
-
 import classes.Triangle;
+import java.util.Scanner;
 
 public class Teste {
 
         public static void main(String[] args) {
-            Triangle t = new Triangle();
-            t.l1 = 3;
-            t.l2 = 4;
-            t.l3 = 5;
-            System.out.println(t.l1);
-            System.out.println(t.l2);
-            System.out.println(t.l3);
+
+        Scanner sc = new Scanner(System.in);
+        
+        //Criação da variavel de objeto da classe Triangle
+        Triangle x,y;
+        x = new Triangle();
+        y = new Triangle();
+        
+        
+        System.out.println("Digite as medidas do triângulo X: ");
+        System.out.println("Lado 1: ");
+        x.l1= sc.nextDouble(); //Entrada dos dados do objeto
+
+        System.out.println("Lado 2: ");
+        x.l2= sc.nextDouble();//Entrada dos dados do objeto
+
+        System.out.println("Lado 3: ");
+        x.l3= sc.nextDouble();//Entrada dos dados do objeto
+
+
+
+        System.out.println("Digite as medidas do triângulo Y: ");
+        System.out.println("Lado 1: ");
+        y.l1 = sc.nextDouble();//Entrada dos dados do objeto
+
+        System.out.println("Lado 2:");
+        y.l2 = sc.nextDouble();//Entrada dos dados do objeto
+
+        System.out.println("Lado 3:");
+        y.l3 = sc.nextDouble();//Entrada dos dados do objeto
+
+        
+        
+        System.out.printf("A área do triangulo x é de %.2f",x.calculateArea());
+        System.out.printf("\nA área do triangulo y é de %.2f",y.calculateArea());
+
+        
+        sc.close();
+
+            
+
+
         }
 
 }
